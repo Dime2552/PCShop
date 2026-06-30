fe0b72910cca8ac58e5d417d3a1bb370faca1c0d
Dime <163345642+Dime2552@users.noreply.github.com>
2026-06-30 14:56:48 +0300

    feat: add admin layout with sidebar

--- pc-shop-ui/src/app/app.routes.ts
+++ pc-shop-ui/src/app/app.routes.ts
@@ -3,12 +3,21 @@ import { CatalogComponent } from './features/catalog/catalog';
 import { CartComponent } from './features/cart/cart';
 import { AuthComponent } from './features/auth/auth';
 import { adminGuard } from './core/guards/admin.guard';
+import { AdminLayoutComponent } from './features/admin/admin-layout/admin-layout';
 import { AdminProductCreate } from './features/admin/admin-product-create/admin-product-create';
 
 export const routes: Routes = [
   { path: '', component: CatalogComponent },
   { path: 'cart', component: CartComponent },
   { path: 'auth', component: AuthComponent },
-  { path: 'admin/product/new', component: AdminProductCreate, canActivate: [adminGuard] },
+  {
+    path: 'admin',
+    component: AdminLayoutComponent,
+    canActivate: [adminGuard],
+    children: [
+      { path: 'product/new', component: AdminProductCreate },
+      { path: '', redirectTo: 'product/new', pathMatch: 'full' }
+    ]
+  },
   { path: '**', redirectTo: '' }
 ];
